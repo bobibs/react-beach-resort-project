6d01e115355dd32f736375451d4b0a8ec022535c
Bobi Basari <bobibasari@gmail.com>
2019-10-31 16:55:57 +0700

    react beach resort project

---
+++ src/pages/Error.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+
+const Error = () => {
+  return (
+    <Hero>
+      <Banner title="404" subtitle="Page Not Found">
+        <Link to="/" className="btn-primary">
+          Return Home
+        </Link>
+      </Banner>
+    </Hero>
+  );
+};
+
+export default Error;
