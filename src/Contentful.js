04c28266ead3d6ce768b217d8c8004a0f37a029d
Bobi Basari <bobibasari@gmail.com>
2019-11-04 12:38:06 +0700

    edit key

--- src/Contentful.js
+++ src/Contentful.js
@@ -1,6 +1,6 @@
 import { createClient } from 'contentful';
 
 export default createClient({
-  space: process.env.REACT_APP_API_SPACE,
-  accessToken: process.env.REACT_APP_ACCESS_TOKEN
+  space: '3wpon9s6ss2x',
+  accessToken: 'Db-XUohzGHBMiv-1O4huA24Er_FNnb8Oh1KF9QyQb4Y'
 });
